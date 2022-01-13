895aefb287729c174f548c4e4c81980836ccbff3
Istip <isticsek@gmail.com>
2022-01-13 18:35:20 +0200

    Created basic stuff

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    mongoose.connect(process.env.DATABASE_URL, connectionParams);
+    console.log('Connected to Database!');
+  } catch (error) {
+    console.log(error);
+    console.log('Could not connect to Database!');
+  }
+};
